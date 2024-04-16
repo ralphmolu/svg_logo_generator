edd5e30a3077356912f2682790331dad1bdec35a
Ralph Molu <rmolu11@gmail.com>
2024-04-15 22:55:41 -0400

    File structure and questions added

--- index.js
+++ index.js
@@ -4,14 +4,33 @@ const fs = require('fs')
 
 //prompt questions
 inquirer.prompt([
-    {},{},{}
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: [Circle, Triangle, Square],
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: input => input.length <= 3,
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a text Color (keyword or hex):'
+    },
+    {
+        type
+    }
 ])
 .then((response) => {
     // destructure response object
     const { } = response;
 
     //creating the logo.svg
-    fs.writeFile('logo.svg', , (error) => {
+    fs.writeFile('logo.svg', 'Image content', (error) => {
         error ? console.log('Error creating the logo') : console.log('logo.svg successfully created!')
     })
 })
